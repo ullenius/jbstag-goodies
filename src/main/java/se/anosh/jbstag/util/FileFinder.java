13b2d95bdc5f2915250036295ae57b98cee6eb1c
Anosh D. Ullenius <anosh@anosh.se>
2021-05-22 10:37:37 +0200

    Add FileFinder util class

---
+++ src/main/java/se/anosh/jbstag/util/FileFinder.java
@@ -0,0 +1,45 @@
+package se.anosh.jbstag.util;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FileFinder extends SimpleFileVisitor<Path> {
+
+    private final PathMatcher matcher;
+    private List<Path> myList = new ArrayList<>();
+
+    FileFinder(String pattern) {
+        matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+    }
+
+    // Compares the glob pattern against
+    // the file or directory name.
+    void match(Path file) throws IOException {
+        Path name = file.getFileName();
+        if (name != null && matcher.matches(name)) {
+            myList.add(file.toAbsolutePath());
+        }
+    }
+
+    // Invoke the pattern matching
+    // method on each file.
+    @Override
+    public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+        match(file);
+        return FileVisitResult.CONTINUE;
+    }
+
+    public List<Path> getList() {
+        return Collections.unmodifiableList(myList);
+    }
+
+    public int getTotalMatches() {
+        return myList.size();
+    }
+
+
+}
